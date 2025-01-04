b9fb0dad800e4a2e0254ee00bf262c3fcb316649
Jason <jason.lim8873@gmail.com>
2025-01-04 16:15:34 +1100

    used OpenWeatherMap API to get weather data

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,51 @@ app.get('/', (req, res) => {
     res.sendFile(VIEWS_FOLDER + 'index.html');
 });
 
+app.post('/getOutfit', async (req, res) => {
+    const location  = req.body.location;
 
+    try {
+        const locationResponse = await axios.get(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${1}&appid=${OPENWEATHER_API_KEY}`
+        );
+
+        const lat = locationResponse.data[0].lat;
+        const lon = locationResponse.data[0].lon;
+
+        // Fetch weather data
+        const weatherResponse = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=metric`
+        );
+        const weatherData = weatherResponse.data;
+        
+        // Extract relevant data
+        const temperature = weatherData.main.temp;
+        const temperature_min = weatherData.main.temp_min;
+        const temperature_max = weatherData.main.temp_max;
+        const humidity = weatherData.main.humidity;
+        const wind_speed = weatherData.wind.speed;
+        const clouds = weatherData.clouds.all;
+        const condition = weatherData.weather[0].description.toLowerCase();
+    
+        // Map to suggested outfit
+        //   const outfit = outfitMapping[condition] || "Default Outfit";
+    
+        res.json({
+            temperature: `${temperature}°C`,
+            temperature_min: `${temperature_min}°C`,
+            temperature_max: `${temperature_max}°C`,
+            humidity: `${humidity}%`,
+            wind_speed: `${wind_speed}m/s`,
+            clouds: `${clouds}%`,
+            condition,
+            // condition,
+            // outfit,
+        });
+        } catch (error) {
+        console.error("Error fetching weather data:", error);
+        res.status(500).json({ error: "Unable to fetch weather data" });
+        }
+    });
 
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
